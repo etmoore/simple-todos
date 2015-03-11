2fad3d9a8626f1c97220381caf957daecf81bd34
Evan Moore <etmoore@gmail.com>
2015-03-11 14:21:42 -0600

    defined task template

--- simple-todos.js
+++ simple-todos.js
@@ -1,28 +1,10 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-  Session.setDefault('foo', 10);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    },
-    foo: function() {
-      return Session.get('foo');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-      Session.set('foo', Session.get('foo') + 2);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" },
+    ]
   });
 }
